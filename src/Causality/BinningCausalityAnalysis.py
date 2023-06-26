4e3ef358fb4025e72388a0df6a825b5c80276fd6
Tianyi Pan <nappan363@gmail.com>
2023-06-26 14:05:23 -0400

    restore causality analysis functionality

--- src/Causality/BinningCausalityAnalysis.py
+++ src/Causality/BinningCausalityAnalysis.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from TS.TimeSeriesBuilder import TimeSeriesBuilder
 from User.UserType import UserType
 from Causality.CausalityAnalysisTool import *
@@ -15,9 +17,6 @@ class BinningCausalityAnalysis:
         self.ts_builder = ts_builder
         self.lags = lags
 
-    def set_lags(self, lags: List[int]) -> None:
-        self.lags = lags
-
     def _plot_lag_to_cause(self, user_type1: UserType, mapping1: str,
                            user_type2: UserType, mapping2: str,
                            cluster_list: List[int], save: bool) -> None:
@@ -25,6 +24,11 @@ class BinningCausalityAnalysis:
         for cluster_id in cluster_list:
             d_series, s_series = self.ts_builder.create_type_series(
                 user_type1, mapping1, user_type2, mapping2, cluster_id)
+
+            # process series
+            # d_series = np.diff(d_series)
+            # s_series = np.diff(s_series)
+
             p_vals = gc_score_for_lags(d_series, s_series, self.lags)
             plt.plot(self.lags, p_vals, label=cluster_id)
         plt.axhline(y=0.05, color="r", linestyle="--")
