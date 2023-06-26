d7b2460a6592371aac433e8390e9d29c2910548d
Tianyi Pan <nappan363@gmail.com>
2023-06-26 14:05:40 -0400

    add new strategy to factory

--- src/UserPartitioning/UserPartitioningStrategyFactory.py
+++ src/UserPartitioning/UserPartitioningStrategyFactory.py
@@ -1,8 +1,11 @@
 from UserPartitioning.UsersStrategy import UsersStrategy
+from UserPartitioning.SocialSupportStrategy import SocialSupportStrategy
 
 
 def get_user_type_strategy(strategy: str):
     if strategy == "users":
         return UsersStrategy()
+    if strategy == "social_support":
+        return SocialSupportStrategy()
     else:
         raise ValueError(f"'{strategy}' does not exist")
