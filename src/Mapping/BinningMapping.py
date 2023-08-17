292866dc6f86580c9abc64319be84b92aff75aef
rachelmkim <rachel.minyoung.kim@gmail.com>
2023-08-17 13:52:56 -0400

    cleaned up old code

--- src/Mapping/BinningMapping.py
+++ src/Mapping/BinningMapping.py
@@ -16,6 +16,7 @@ class BinningMapping(ContentTypeMapping):
     def __init__(self, args: Dict[str, Any]):
         super().__init__(args)
 
+        ### Unfiltered #############################################################################
         embeddings = args["embeddings"]
         num_bins = args["num_bins"]
 
@@ -34,45 +35,7 @@ class BinningMapping(ContentTypeMapping):
 
         self.bins = [self._find_bin_number(pca_value) for pca_value in pca.components_[0]]
 
-        # (3) - remove ids from bins that are junk, and re-bin (uncomment the above)
-        # embeddings = args["embeddings"]
-        # num_bins = args["num_bins"]
-
-        # # PCA onto one dimension
-        # X = np.asarray(list(embeddings.values()), dtype=np.float32)
-
-        # self.ids = list(embeddings.keys())
-
-        # pca = PCA(n_components=1)
-        # pca.fit(np.transpose(np.asarray(X)))
-
-        # min_pca, max_pca = min(pca.components_[0]), max(pca.components_[0])
-        # bin_size = (max_pca - min_pca) / num_bins
-
-        # self.bin_boundaries = [min_pca + i * bin_size for i in range(num_bins - 1)] + [max_pca]
-
-        # self.bins = [self._find_bin_number(pca_value) for pca_value in pca.components_[0]]
-
-        # new_ids, new_values = [], []
-        # for i in range(len(self.ids)):
-        #     if self.bins[i] < 13:
-        #         new_ids.append(self.ids[i])
-        #         new_values.append(embeddings[self.ids[i]])
-        # X = np.asarray(new_values, dtype=np.float32)
-
-        # self.ids = new_ids
-
-        # pca = PCA(n_components=1)
-        # pca.fit(np.transpose(np.asarray(X)))
-
-        # min_pca, max_pca = min(pca.components_[0]), max(pca.components_[0])
-        # bin_size = (max_pca - min_pca) / num_bins
-
-        # self.bin_boundaries = [min_pca + i * bin_size for i in range(num_bins - 1)] + [max_pca]
-
-        # self.bins = [self._find_bin_number(pca_value) for pca_value in pca.components_[0]]
-
-        # (4) - filter
+        ### Filtered (uncomment below) ############################################################
         # embeddings = args["embeddings"]
         # num_bins = args["num_bins"]
 
@@ -81,7 +44,9 @@ class BinningMapping(ContentTypeMapping):
         # original_tweet_ids = {tweet.id for tweet in self.market.original_tweets}
         # retweets_of_in_comm_ids = {tweet.id for tweet in self.market.retweets_of_in_comm}
         # retweets_of_out_comm_ids = {tweet.id for tweet in self.market.retweets_of_out_comm}
-        # all_ids = original_tweet_ids.union(retweets_of_in_comm_ids.union(retweets_of_out_comm_ids))
+        # retweets_of_out_comm_by_in_comm_ids = {tweet.id for tweet in self.market.retweets_of_out_comm_by_in_comm}
+        # # all_ids = original_tweet_ids.union(retweets_of_in_comm_ids.union(retweets_of_out_comm_ids.union(retweets_of_out_comm_by_in_comm_ids)))
+        # all_ids = original_tweet_ids.union(retweets_of_in_comm_ids)
 
         # new_embeddings = {}
         # for tweet_id in embeddings:
